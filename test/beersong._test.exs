70b2cfea85372b954ac5ded469d11bcd5ce8a9f7
Herbew <herbew@gmail.com>
2022-04-27 22:18:41 +0700

    test 2 bottles

--- test/beersong._test.exs
+++ test/beersong._test.exs
@@ -1,11 +1,26 @@
 defmodule BeerSongTest do
     use ExUnit.Case
-	test "verse with 0 bottles" do
-      assert BeerSong.verse(0) == \
-      	"No more bottles of beer on the wall, no more bottles of beer.\n" <> \
-		"Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-    end #test "verse with 0 bottles" do
-	
+    
+    describe "verse" do
+		test "verse with 0 bottles" do
+	      assert BeerSong.verse(0) == \
+	      	"No more bottles of beer on the wall, no more bottles of beer.\n" <> \
+			"Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+	    end #test "verse with 0 bottles" do
+	    
+	    test "verse with 1 bottles" do
+	      assert BeerSong.verse(1) == \
+	      	"1 bottle of beer on the wall, 1 bottle of beer.\n" <> \
+			"Take it down and pass it around, no more bottles of beer on the wall.\n"
+	    end #test "verse with 1 bottles" do
+	    
+	    test "verse with 2 bottles" do
+	      assert BeerSong.verse(2) == \
+	      	"2 of beer on the wall, 2 of beer.\n" <> \
+			"Take one down and pass it around, 1 of beer on the wall.\n"
+	    end #test "verse with 2 bottles" do
+	    
+	end #describe "verse" do
 	
 	
 end
