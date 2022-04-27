b5d16a58b683dea0a0c669a87599914e76ed8abe
Herbew <herbew@gmail.com>
2022-04-27 22:05:11 +0700

    verse test 0

--- test/beersong._test.exs
+++ test/beersong._test.exs
@@ -1,10 +1,7 @@
 defmodule BeerSongTest do
     use ExUnit.Case
 	test "verse with 0 bottles" do
-      assert BeerSong.verse(0) == """
-             No more bottles of beer on the wall, no more bottles of beer.
-    		 Go to the store and buy some more, 99 bottles of beer on the wall.
-             """
+      assert BeerSong.verse(0) == "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     end #test "verse with 0 bottles" do
 	
 	
